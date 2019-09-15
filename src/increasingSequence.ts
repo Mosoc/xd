fb3505dd958430668d6565d9fae07d92253b64b7
Bruno Anderson Chen <mosoc@colle.co>
2019-09-15 23:07:24 +0900

    Pick up arithmetic progression

--- src/increasingSequence.ts
+++ src/increasingSequence.ts
@@ -1,9 +1,27 @@
-const increasingSequence = (sequence: number[]): number[] => {
+const increasingSequence = (
+  sequence: number[],
+  extending: number = 10
+): number[] => {
   if (!sequence || sequence.length === 0) {
     return [];
   }
   if (sequence.every(element => element === sequence[0])) {
-    return Array(10).fill(sequence[0]);
+    return Array(extending).fill(sequence[0]);
   }
+  //  Get differences in the array
+  const differences: number[] = [];
+  for (let index = 1; index < sequence.length; index++) {
+    differences.push(sequence[index] - sequence[index - 1]);
+  }
+
+  //  Arithmetic progression
+  if (differences.every(element => element === differences[0])) {
+    return Array(extending)
+      .fill(sequence[0])
+      .map((value, index) => value + index * differences[0]);
+  }
+
   return [];
 };
+
+// console.log(increasingSequence([1, 2, 3]));

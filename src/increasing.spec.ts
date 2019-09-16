6dcee5fc238da131e9e2f238102070f886d1dd7b
Bruno Anderson Chen <mosoc@colle.co>
2019-09-16 10:10:31 +0900

    Update specification for nested statements

--- src/increasing.spec.ts
+++ src/increasing.spec.ts
@@ -37,4 +37,41 @@ describe('Increasing sequence', () => {
     );
     expect(result).is.deep.equal([1, 2, 4, 8, 16]);
   });
+
+  it('Difference sequence is arithmetic progression', () => {
+    const resultLength = 5;
+    const result = increasingSequence(
+      [1, 2, 4, 7],
+      resultLength
+    );
+    expect(result).is.deep.equal([1, 2, 4, 7, 11]);
+  });
+
+  it('Difference sequence is geometric progression', () => {
+    const resultLength = 5;
+    const result = increasingSequence(
+      [1, 3, 7, 15],
+      resultLength
+    );
+    expect(result).is.deep.equal([1, 3, 7, 15, 31]);
+  });
+
+  it('Radio sequence is arithmetic progression', () => {
+    const resultLength = 5;
+    const result = increasingSequence(
+      [1, 2, 6, 24],
+      resultLength
+    );
+    expect(result).is.deep.equal([1, 2, 6, 24, 720]);
+  });
+
+  it('Radio sequence is geometric progression', () => {
+    const resultLength = 5;
+    const result = increasingSequence(
+      [1, 2, 8, 64],
+      resultLength
+    );
+    expect(result).is.deep.equal([1, 2, 8, 64, 1024]);
+  });
+
 });
